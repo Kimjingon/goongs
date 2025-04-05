040633a79a823f071d23d7346052e742a6dbeffd
navid.k(김진곤)/kakao <navid.k@kakaocorp.com>
2025-04-05 17:31:14 +0900

    add car cnt and crwal

--- job/crawling.py
+++ job/crawling.py
@@ -7,30 +7,35 @@ from selenium.webdriver.chrome.options import Options
 
 from client.mysql_client import MysqlClient
 
-if __name__ == "__main__":
 
+def get_car_cnt_data():
+    url = "https://kosis.kr/visual/nsportalStats/detailContents.do?statJipyoId=3707&listId=L&vStatJipyoId=5222"
+    driver.get(url)
+    time.sleep(2)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    table = soup.find("table", class_="bd_lst_tbl")
+
+    data = []
+    tr = table.find("tbody").find("tr")
+    for td in tr.find_all("td")[1:]:
+        key = td["data-cell-header"].replace(".", "-")
+        value = td.get_text(strip=True).replace(",", "")
+        data.append((key, int(value)))
+
+    return data
+
+
+
+if __name__ == "__main__":
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
 
     driver = webdriver.Chrome(options=options)
-    url = "https://kosis.kr/visual/nsportalStats/detailContents.do?statJipyoId=3707&listId=L&vStatJipyoId=5222"
-    driver.get(url)
 
-    time.sleep(2)
-
-    html = driver.page_source
-
-    soup = BeautifulSoup(html, "html.parser")
-    table = soup.find("table", class_="bd_lst_tbl")
-
-    rows = []
-    tr = table.find("tbody").find("tr")
-    for td in tr.find_all("td")[1:]:
-        key = td["data-cell-header"].replace(".", "-")
-        value = td.get_text(strip=True).replace(",", "")
-        rows.append((key, int(value)))
-    driver.quit()
+    columns = ["year_month_id", "cnt"]
+    table = "car_cnt_info"
 
     db = MysqlClient(
         host=st.secrets["db"]["host"],
@@ -38,8 +43,7 @@ if __name__ == "__main__":
         password=st.secrets["db"]["password"],
         database=st.secrets["db"]["database"],
     )
-    columns = ["year_month_id", "cnt"]
-    table = "car_cnt_info"
-    print(rows)
-    db.insert_data(data=rows, columns=columns, table=table)
+    car_cnt_data = get_car_cnt_data()
+    db.insert_data(data=car_cnt_data, columns=columns, table=table)
     db.close()
+    driver.quit()
